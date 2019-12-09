afd0e3e36fe1a30bbef35375f166ea4876eaf3a2
Chander batth <chander batth @ local pc . com>
2019-12-09 16:36:26 +0000

    crud ops done .. new customers add .. no prompt,,,

--- nodejs-mysql-crud/src/controllers/customerController.js
+++ nodejs-mysql-crud/src/controllers/customerController.js
@@ -13,16 +13,12 @@ controller.list = (req, res) => {
     });
 };
 
-controller.save = (req, res) => {
-    const data = req.body;
-    console.log(req.body);
-    req.getConnection((err, connection) => {
-        const query = connection.query('INSERT INTO customer set ?', data, (err, customer) => {
-            console.log(customer);
-            res.redirect('/');
-        })
-    })
+controller.new = (req, res) => {
+    //const {id} = 991199;
+    res.render('customers_new', null );
+        
 };
+
 
 controller.edit = (req, res) => {
     const {id} = req.params;
@@ -35,12 +31,29 @@ controller.edit = (req, res) => {
     });
 };
 
+controller.save = (req, res) => {
+    console.log(req.body);
+    const newCustomer = req.body;
+    req.getConnection((err, conn) => {
+        // const query = connection.query('INSERT INTO customer set ?', data, (err, customer) => {
+        conn.query('INSERT INTO bookmarks set ?', [newCustomer], (err, rows) => {    
+            console.log(newCustomer);
+            res.redirect('/');
+        })
+    })
+};
+
 controller.update = (req, res) => {
     const {id} = req.params;
     const newCustomer = req.body;
     req.getConnection((err, conn) => {
-
-        conn.query('UPDATE customer set ? where id = ?', [newCustomer, id], (err, rows) => {
+        // UPDATE bookmarks SET Catagory = 'SysAdmin' , Details = 'Sys-con1' , Url = 'http://www.sys-con.com/' WHERE ID='100225'
+        // conn.query('UPDATE bookmarks set ? where id = ?', [newCustomer, id], (err, rows) => {
+        conn.query('UPDATE bookmarks set Category = ?, Details = ?, Url = ? where id = ?', [newCustomer, id], (err, rows) => {
+            if (err) {
+                console.log(req.body);
+                res.json(err);
+            }
             res.redirect('/');
         });
     });
